791796340f4d5080916ecd9d3a734a267a33bd22
Mari Yamamoto <mariana.mhy@gmail.com>
2022-07-05 09:05:01 -0400

    Add detail page and sort descriptors

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
-    @FetchRequest(sortDescriptors: []) var books: FetchedResults<Book>
+    @FetchRequest(sortDescriptors: [
+        SortDescriptor(\.title),
+        SortDescriptor(\.author)
+    ]) var books: FetchedResults<Book>
 
     @State private var showingAddScreen = false
 
@@ -18,7 +21,7 @@ struct ContentView: View {
             List {
                 ForEach(books) { book in
                     NavigationLink {
-                        Text(book.title ?? "Unknown Title")
+                        DetailView(book: book)
                     } label: {
                         HStack {
                             EmojiRatingView(rating: book.rating)
